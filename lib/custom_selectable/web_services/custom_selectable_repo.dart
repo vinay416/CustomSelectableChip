60c2cd8fc09dedddb561e3d848e33e04b7ebfc8c
ruturaj.srashtasoft <ruturaj.srashtasoft@5gencare.com>
2022-09-07 17:42:30 +0530

    token changed

--- lib/custom_selectable/web_services/custom_selectable_repo.dart
+++ lib/custom_selectable/web_services/custom_selectable_repo.dart
@@ -1,6 +1,7 @@
 import 'package:custom_chip/custom_selectable/model/skill_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class CustomSelectableRepo {
   Future<List<SkillModel>> getSkills(String apiUrl) async {
@@ -14,13 +15,18 @@ class CustomSelectableRepo {
       if (response.statusCode == 200) {
         final data = response.data;
         final List<dynamic> skills = data["result"];
+        if (skills.isEmpty) {
+          _showToast("Error : ${response.data["message"]}");
+        }
         final List<SkillModel> list =
             skills.map((e) => SkillModel.fromJson(e)).toList();
         return list;
       }
+      _showToast("Error : ${response.statusCode}");
       return [];
     } on Exception catch (e) {
       debugPrint(e.toString());
+      _showToast("Error : $e");
     }
     return [];
   }
@@ -28,8 +34,14 @@ class CustomSelectableRepo {
   Map<String, dynamic> get _authHeader {
     return {
       "content-type": "application/json",
-      "Authorization":
-          "Basic eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiclhnY1Y2YXh3eVRobTNQdE04aGtSaXJTQ2ZsMiIsImlhdCI6MTY2MjQ1ODUyNywiZXhwIjoxNjYyNTQ0OTI3fQ.ihNAgDyt3miZz9opJoNbQSlG21A4TMMOhNkKDbZBMFo"
+      "Authorization": "Basic $_token"
     };
+  }
+
+  final String _token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiclhnY1Y2YXh3eVRobTNQdE04aGtSaXJTQ2ZsMiIsImlhdCI6MTY2MjU1MTgwOCwiZXhwIjoxNjYyNjM4MjA4fQ.RGJM_0Iqfcq-WZi6Kqtj25eso5t7sdoDxUBjRySGoLI";
+
+  void _showToast(String message) {
+    Fluttertoast.showToast(msg: message);
   }
 }
